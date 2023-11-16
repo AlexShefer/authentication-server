141e599c0a829e951e645e67b99b00befcb34c23
Aleksandr <Avshef451987@gmail.com>
2023-11-16 14:22:38 +0500

    fix bags

--- server.js
+++ server.js
@@ -9,31 +9,23 @@ const userRoutes = require("./routes/user");
 const app = express();
 
 // middleware
+mongoose.set("strictQuery", false);
 app.use(express.json());
 app.use(cors());
-
-app.use((req, res, next) => {
-    console.log(req.path, req.method);
-    next();
-});
-
-// routes
-app.use("/api/user", userRoutes);
-
-mongoose.set("strictQuery", false);
-// connect to db
+// connect  db
 mongoose
-
     .connect(process.env.MONGO_URI)
     .then(() => {
         // listen for requests
-        app.listen(process.env.PORT, () => {
-            console.log(
-                "connected to db & listening on port",
-                process.env.PORT
-            );
-        });
+        console.log("the database is connected");
     })
     .catch((error) => {
         console.log(error);
     });
+
+// routes
+app.use("/api/user", userRoutes);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+});
